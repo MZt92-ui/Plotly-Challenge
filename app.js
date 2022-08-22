dd148aa942bb776b6e9e409e72198d4bd30b0c2a
MZt92-ui <76995395+MZt92-ui@users.noreply.github.com>
2022-08-22 10:55:06 +1000

    temp

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 
-d3.json("samples.json").then((importedData) => {
-    console.log(importedData);
-});
+
+function init(){
+
+    d3.json("samples.json").then((importedData) => {
+
+    init_sample_value = importedData.samples[0]["sample_values"];
+    init_otu = importedData.samples[0]["otu_ids"];
+        
+    var trace = {
+        x: init_otu,
+        y: init_sample_value,
+        type: "bar",
+        };
+    init_bar = [trace];
+    Plotly.newPlot("bar",init_bar);
+    });
+};
+
+init();
+
+
